7f4987af5e7959412aafacb617cb515a70c61de2
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-09-03 23:24:03 +0700

    took a logger in Controller math.

--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { MathService } from './math.service';
 
 @Controller('math')
 export class MathController {
   constructor(private readonly mathService: MathService) {}
+  private logger = new Logger();
 
   @Post('sum')
   sum(@Body('numbers') numbers: number[]): number {
+    this.logger.log('Sum: ' + numbers);
     return this.mathService.accumulate(numbers);
   }
 }
