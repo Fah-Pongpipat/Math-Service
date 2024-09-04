2a51ff9aa613d6e3fdcc4aa0ebe4447d2f981aa6
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-09-04 15:31:42 +0700

    Configured Math Server.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
+
+  const microservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 8877,
+      },
+    });
+  await microservice.listen();
 }
 bootstrap();
